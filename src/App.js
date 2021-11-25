54f4542314780f428bc813c83dbeaf8be2b8110b
Vincent Nguyen <vpvnguyen@gmail.com>
2021-11-25 12:01:51 -0800

    add base styles; create custom filter table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import FilterTable from "./components/filter";
+import styled from "styled-components";
+import FilterTable from "./components/Filter";
+import CustomFilterTable from "./components/CustomFilter";
+
+const Main = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+`;
+const Section = styled.section`
+  border: 1px solid grey;
+`;
 
 const App = () => {
   return (
-    <div>
-      <h1>FilterTable</h1>
-      <FilterTable />
-    </div>
+    <Main>
+      <Section>
+        <h1>FilterTable</h1>
+        <FilterTable />
+      </Section>
+      <Section>
+        <h1>CustomFilterTable</h1>
+        <CustomFilterTable />
+      </Section>
+    </Main>
   );
 };
 
